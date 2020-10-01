eea5217961dc73df1a96e2d7a688f3270e3083b0
Constanza Miño <mcmino@uc.cl>
2020-09-30 23:16:46 -0300

    obtener y listar proveedores, insumos, unidades de medida

--- Restaurant.Web/Paginas/Administrador/GestionInventario.aspx.cs
+++ Restaurant.Web/Paginas/Administrador/GestionInventario.aspx.cs
@@ -1,21 +1,67 @@
-﻿using System;
+﻿using Restaurant.Services.Servicios;
+using Restaurant.Model.Clases;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Collections;
 
 namespace Restaurant.Web.Paginas.Administrador
 {
     public partial class GestionInventario : System.Web.UI.Page
     {
+        private ProveedorService _proveedorService;
+        private InsumoService _insumoService;
+        private UnidadDeMedidaService _unidadDeMedidaService;
+
         protected void Page_Load(object sender, EventArgs e)
         {
             if (!IsPostBack)
             {
                 ValidarSesion();
-                //Llenar proveedores
-                //Llenar unidades de medida
+                _proveedorService = new ProveedorService();
+                _insumoService = new InsumoService();
+                _unidadDeMedidaService = new UnidadDeMedidaService();
+
+                List<Proveedor> proveedores = _proveedorService.Obtener();
+                if (proveedores != null)
+                {
+                    if (proveedores.Count > 0)
+                    {
+                        listaProveedores.DataSource = proveedores;
+                        listaProveedores.DataBind();
+
+                        ddlProveedorInsumo.DataSource = proveedores;
+                        ddlProveedorInsumo.DataTextField = "NombreProveedor";
+                        ddlProveedorInsumo.DataValueField = "Id"; 
+                        ddlProveedorInsumo.DataBind();
+                        ddlProveedorInsumo.Items.Insert(0, new ListItem("Seleccionar", ""));
+                        ddlProveedorInsumo.SelectedIndex = 0;
+                    }
+                }
+
+                List<Insumo> insumos = _insumoService.Obtener();
+                if (insumos != null)
+                {
+                    listaInsumos.DataSource = insumos;
+                    listaInsumos.DataBind();
+                }
+
+                List<UnidadMedida> unidades = _unidadDeMedidaService.Obtener();
+                if (unidades != null)
+                {
+                    if (unidades.Count > 0)
+                    {
+                        ddlProveedorInsumo.DataSource = unidades;
+                        ddlProveedorInsumo.DataTextField = "Nombre";
+                        ddlProveedorInsumo.DataValueField = "Id";
+                        ddlProveedorInsumo.DataBind();
+                        ddlProveedorInsumo.Items.Insert(0, new ListItem("Seleccionar", ""));
+                        ddlProveedorInsumo.SelectedIndex = 0;
+                    }
+                }
             }
         }
 
