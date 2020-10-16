3d935a4d48ab3e598e8b5c0ec7b4d1e07739425d
Constanza Miño <mcmino@uc.cl>
2020-10-15 22:33:15 -0300

    vistas principales de reserva y autoservicio

--- Restaurant.Web/Paginas/Publica/Autoservicio.aspx.cs
+++ Restaurant.Web/Paginas/Publica/Autoservicio.aspx.cs
@@ -7,19 +7,14 @@ using System.Web.UI.WebControls;
 
 namespace Restaurant.Web.Paginas.Publica
 {
-    public partial class Reservas : System.Web.UI.Page
+    public partial class Autoservicio : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
 
         }
 
-        protected void btnReservarMesa_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        protected void btnCancelarReserva_Click(object sender, EventArgs e)
+        protected void btnAutoservicio_Click(object sender, EventArgs e)
         {
 
         }
