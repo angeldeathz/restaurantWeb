f84af513ee6e77e9691f157963dbec7b8f79a551
Constanza Miño <mcmino@uc.cl>
2020-12-08 17:40:56 -0300

    try catch gestion usuarios

--- Restaurant.Web/Paginas/Mantenedores/GestionUsuarios.aspx.cs
+++ Restaurant.Web/Paginas/Mantenedores/GestionUsuarios.aspx.cs
@@ -17,30 +17,38 @@ namespace Restaurant.Web.Paginas.Mantenedores
 
         protected void Page_Load(object sender, EventArgs e)
         {
-            if (!IsPostBack)
+            try
             {
-                ValidarSesion();
-
-                Token token = (Token)Session["token"];
-                _usuarioService = new UsuarioService(token.access_token);
-                _tipoUsuarioService = new TipoUsuarioService(token.access_token);
-
-                List<Usuario> usuarios = _usuarioService.Obtener();
-                if (usuarios != null && usuarios.Count > 0)
+                if (!IsPostBack)
                 {
-                    actualizarRepeater(listaUsuarios, usuarios, listaUsuariosVacia);
-                }
+                    ValidarSesion();
 
-                List<TipoUsuario> tipoUsuarios = _tipoUsuarioService.Obtener();
-                if (tipoUsuarios != null && tipoUsuarios.Count > 0)
-                {
-                    ddlTipoUsuario.DataSource = tipoUsuarios;
-                    ddlTipoUsuario.DataTextField = "Nombre";
-                    ddlTipoUsuario.DataValueField = "Id";
-                    ddlTipoUsuario.DataBind();
-                    ddlTipoUsuario.Items.Insert(0, new ListItem("Seleccionar", ""));
-                    ddlTipoUsuario.SelectedIndex = 0;
+                    Token token = (Token)Session["token"];
+                    _usuarioService = new UsuarioService(token.access_token);
+                    _tipoUsuarioService = new TipoUsuarioService(token.access_token);
+
+                    List<Usuario> usuarios = _usuarioService.Obtener();
+                    if (usuarios != null && usuarios.Count > 0)
+                    {
+                        actualizarRepeater(listaUsuarios, usuarios, listaUsuariosVacia);
+                    }
+
+                    List<TipoUsuario> tipoUsuarios = _tipoUsuarioService.Obtener();
+                    if (tipoUsuarios != null && tipoUsuarios.Count > 0)
+                    {
+                        ddlTipoUsuario.DataSource = tipoUsuarios;
+                        ddlTipoUsuario.DataTextField = "Nombre";
+                        ddlTipoUsuario.DataValueField = "Id";
+                        ddlTipoUsuario.DataBind();
+                        ddlTipoUsuario.Items.Insert(0, new ListItem("Seleccionar", ""));
+                        ddlTipoUsuario.SelectedIndex = 0;
+                    }
                 }
+            }
+            catch (Exception ex)
+            {
+                ScriptManager.RegisterStartupScript(Page, Page.GetType(), "error", "Swal.fire('Error', 'No se pudo cargar la página. Intente nuevamente', 'error');", true);
+                return;
             }
         }
 
@@ -88,36 +96,44 @@ namespace Restaurant.Web.Paginas.Mantenedores
         }
         protected void btnModalEditarUsuario_Click(object source, RepeaterCommandEventArgs e)
         {
-            ValidarSesion();
-            int idUsuario;
-            if (int.TryParse((string)e.CommandArgument, out idUsuario))
+            try
             {
-                Token token = (Token)Session["token"];
-                _usuarioService = new UsuarioService(token.access_token);
-                Usuario usuario = _usuarioService.Obtener(idUsuario);
-                if (usuario != null)
+                ValidarSesion();
+                int idUsuario;
+                if (int.TryParse((string)e.CommandArgument, out idUsuario))
                 {
-                    tituloModalUsuario.Text = "Editar Usuario";
-                    btnCrearUsuario.Visible = false;
-                    btnEditarUsuario.Visible = true;
-                    txtIdUsuario.Text = usuario.Id.ToString();
-                    txtNombreUsuario.Text = usuario.Persona.Nombre;
-                    txtApellidoUsuario.Text = usuario.Persona.Apellido;
-                    txtRutUsuario.Text = usuario.Persona.Rut.ToString();
-                    txtDigitoVerificadorUsuario.Text = usuario.Persona.DigitoVerificador.ToString();
-                    txtEmailUsuario.Text = usuario.Persona.Email;
-                    txtTelefonoUsuario.Text = usuario.Persona.Telefono.ToString();
-                    ddlTipoUsuario.SelectedValue = usuario.IdTipoUsuario.ToString();
-                    txtContrasena.Text = "";
-                    txtContrasenaRepetir.Text = "";
+                    Token token = (Token)Session["token"];
+                    _usuarioService = new UsuarioService(token.access_token);
+                    Usuario usuario = _usuarioService.Obtener(idUsuario);
+                    if (usuario != null)
+                    {
+                        tituloModalUsuario.Text = "Editar Usuario";
+                        btnCrearUsuario.Visible = false;
+                        btnEditarUsuario.Visible = true;
+                        txtIdUsuario.Text = usuario.Id.ToString();
+                        txtNombreUsuario.Text = usuario.Persona.Nombre;
+                        txtApellidoUsuario.Text = usuario.Persona.Apellido;
+                        txtRutUsuario.Text = usuario.Persona.Rut.ToString();
+                        txtDigitoVerificadorUsuario.Text = usuario.Persona.DigitoVerificador.ToString();
+                        txtEmailUsuario.Text = usuario.Persona.Email;
+                        txtTelefonoUsuario.Text = usuario.Persona.Telefono.ToString();
+                        ddlTipoUsuario.SelectedValue = usuario.IdTipoUsuario.ToString();
+                        txtContrasena.Text = "";
+                        txtContrasenaRepetir.Text = "";
 
-                    ScriptManager.RegisterStartupScript(Page, Page.GetType(), "modalUsuario", "$('#modalUsuario').modal('show');", true);
-                    upModalUsuario.Update();
+                        ScriptManager.RegisterStartupScript(Page, Page.GetType(), "modalUsuario", "$('#modalUsuario').modal('show');", true);
+                        upModalUsuario.Update();
+                    }
                 }
+                limpiarTabs();
+                tabUsuarios.Attributes.Add("class", "nav-link active");
+                divUsuarios.Attributes.Add("class", "tab-pane fade active show");
             }
-            limpiarTabs();
-            tabUsuarios.Attributes.Add("class", "nav-link active");
-            divUsuarios.Attributes.Add("class", "tab-pane fade active show");
+            catch (Exception ex)
+            {
+                ScriptManager.RegisterStartupScript(Page, Page.GetType(), "error", "Swal.fire('Error', '" + ex.Message + "', 'error');", true);
+                return;
+            }
         }
 
         protected void btnCrearUsuario_Click(object sender, EventArgs e)
@@ -138,36 +154,44 @@ namespace Restaurant.Web.Paginas.Mantenedores
                 return;
             }
 
-            Usuario usuario = new Usuario();
-            usuario.Persona = new Persona();
-
-            usuario.Persona.Nombre = txtNombreUsuario.Text;
-            usuario.Persona.Apellido = txtApellidoUsuario.Text;
-            usuario.Persona.Rut = int.Parse(txtRutUsuario.Text);
-            usuario.Persona.DigitoVerificador = txtDigitoVerificadorUsuario.Text;
-            usuario.Persona.Email = txtEmailUsuario.Text;
-            usuario.Persona.Telefono = int.Parse(txtTelefonoUsuario.Text);
-            usuario.IdTipoUsuario = int.Parse(ddlTipoUsuario.SelectedValue);
-            usuario.Contrasena = txtContrasena.Text;
-
-            Token token = (Token)Session["token"];
-            _usuarioService = new UsuarioService(token.access_token);
-            int idUsuario = _usuarioService.Guardar(usuario);
-
-            if (idUsuario != 0)
+            try
             {
-                List<Usuario> usuarios = _usuarioService.Obtener();
-                if (usuarios != null && usuarios.Count > 0)
+                Usuario usuario = new Usuario();
+                usuario.Persona = new Persona();
+
+                usuario.Persona.Nombre = txtNombreUsuario.Text;
+                usuario.Persona.Apellido = txtApellidoUsuario.Text;
+                usuario.Persona.Rut = int.Parse(txtRutUsuario.Text);
+                usuario.Persona.DigitoVerificador = txtDigitoVerificadorUsuario.Text;
+                usuario.Persona.Email = txtEmailUsuario.Text;
+                usuario.Persona.Telefono = int.Parse(txtTelefonoUsuario.Text);
+                usuario.IdTipoUsuario = int.Parse(ddlTipoUsuario.SelectedValue);
+                usuario.Contrasena = txtContrasena.Text;
+
+                Token token = (Token)Session["token"];
+                _usuarioService = new UsuarioService(token.access_token);
+                int idUsuario = _usuarioService.Guardar(usuario);
+
+                if (idUsuario != 0)
                 {
-                    actualizarRepeater(listaUsuarios, usuarios, listaUsuariosVacia);
-                    upListaUsuarios.Update();
+                    List<Usuario> usuarios = _usuarioService.Obtener();
+                    if (usuarios != null && usuarios.Count > 0)
+                    {
+                        actualizarRepeater(listaUsuarios, usuarios, listaUsuariosVacia);
+                        upListaUsuarios.Update();
+                    }
+                    ScriptManager.RegisterStartupScript(Page, Page.GetType(), "crearUsuario", "Swal.fire('Usuario creado', '', 'success');", true);
+                    ScriptManager.RegisterStartupScript(Page, Page.GetType(), "modalUsuario", "$('#modalUsuario').modal('hide');", true);
                 }
-                ScriptManager.RegisterStartupScript(Page, Page.GetType(), "crearUsuario", "Swal.fire('Usuario creado', '', 'success');", true);
-                ScriptManager.RegisterStartupScript(Page, Page.GetType(), "modalUsuario", "$('#modalUsuario').modal('hide');", true);
+                else
+                {
+                    ScriptManager.RegisterStartupScript(Page, Page.GetType(), "modalUsuario", "Swal.fire('Error al crear usuario', '', 'error');", true);
+                }
             }
-            else
+            catch (Exception ex)
             {
-                ScriptManager.RegisterStartupScript(Page, Page.GetType(), "modalUsuario", "Swal.fire('Error al crear usuario', '', 'error');", true);
+                ScriptManager.RegisterStartupScript(Page, Page.GetType(), "error", "Swal.fire('Error', '" + ex.Message + "', 'error');", true);
+                return;
             }
         }
 
@@ -189,35 +213,43 @@ namespace Restaurant.Web.Paginas.Mantenedores
                 return;
             }
 
-            Usuario usuario = new Usuario();
-            usuario.Persona = new Persona();
-            usuario.Id = int.Parse(txtIdUsuario.Text);
-            usuario.Persona.Nombre = txtNombreUsuario.Text;
-            usuario.Persona.Apellido = txtApellidoUsuario.Text;
-            usuario.Persona.Rut = int.Parse(txtRutUsuario.Text);
-            usuario.Persona.DigitoVerificador = txtDigitoVerificadorUsuario.Text;
-            usuario.Persona.Email = txtEmailUsuario.Text;
-            usuario.Persona.Telefono = int.Parse(txtTelefonoUsuario.Text);
-            usuario.IdTipoUsuario = int.Parse(ddlTipoUsuario.SelectedValue);
-            usuario.Contrasena = txtContrasena.Text;
+            try
+            {
+                Usuario usuario = new Usuario();
+                usuario.Persona = new Persona();
+                usuario.Id = int.Parse(txtIdUsuario.Text);
+                usuario.Persona.Nombre = txtNombreUsuario.Text;
+                usuario.Persona.Apellido = txtApellidoUsuario.Text;
+                usuario.Persona.Rut = int.Parse(txtRutUsuario.Text);
+                usuario.Persona.DigitoVerificador = txtDigitoVerificadorUsuario.Text;
+                usuario.Persona.Email = txtEmailUsuario.Text;
+                usuario.Persona.Telefono = int.Parse(txtTelefonoUsuario.Text);
+                usuario.IdTipoUsuario = int.Parse(ddlTipoUsuario.SelectedValue);
+                usuario.Contrasena = txtContrasena.Text;
 
-            Token token = (Token)Session["token"];
-            _usuarioService = new UsuarioService(token.access_token);
-            bool editar = _usuarioService.Modificar(usuario, usuario.Id);
-            if (editar)
-            {
-                List<Usuario> usuarios = _usuarioService.Obtener();
-                if (usuarios != null && usuarios.Count > 0)
+                Token token = (Token)Session["token"];
+                _usuarioService = new UsuarioService(token.access_token);
+                bool editar = _usuarioService.Modificar(usuario, usuario.Id);
+                if (editar)
                 {
-                    actualizarRepeater(listaUsuarios, usuarios, listaUsuariosVacia);
-                    upListaUsuarios.Update();
+                    List<Usuario> usuarios = _usuarioService.Obtener();
+                    if (usuarios != null && usuarios.Count > 0)
+                    {
+                        actualizarRepeater(listaUsuarios, usuarios, listaUsuariosVacia);
+                        upListaUsuarios.Update();
+                    }
+                    ScriptManager.RegisterStartupScript(Page, Page.GetType(), "editarUsuario", "Swal.fire('Usuario editado', '', 'success');", true);
+                    ScriptManager.RegisterStartupScript(Page, Page.GetType(), "modalUsuario", "$('#modalUsuario').modal('hide');", true);
                 }
-                ScriptManager.RegisterStartupScript(Page, Page.GetType(), "editarUsuario", "Swal.fire('Usuario editado', '', 'success');", true);
-                ScriptManager.RegisterStartupScript(Page, Page.GetType(), "modalUsuario", "$('#modalUsuario').modal('hide');", true);
+                else
+                {
+                    ScriptManager.RegisterStartupScript(Page, Page.GetType(), "modalUsuario", "Swal.fire('Error al editar usuario', '', 'error');", true);
+                }
             }
-            else
+            catch (Exception ex)
             {
-                ScriptManager.RegisterStartupScript(Page, Page.GetType(), "modalUsuario", "Swal.fire('Error al editar usuario', '', 'error');", true);
+                ScriptManager.RegisterStartupScript(Page, Page.GetType(), "error", "Swal.fire('Error', '" + ex.Message + "', 'error');", true);
+                return;
             }
         }
         public void actualizarRepeater<T>(Repeater repeater, List<T> listaData, Label mensajeListaVacia)
